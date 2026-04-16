f755ab98a4271be6c0c87fbb36a8b1a2b35c4c1d
Abhishek Goyal <goyalabhishek044@gmail.com>
2026-04-16 10:36:57 +0530

    chore: restructure and rename project folders

--- tests/data_structure/test_doubly_ll.py
+++ tests/data_structure/test_doubly_ll.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.data_structures.linked_list.doubly_ll import DoublyLL
+from src.data_structure.linked_list.doubly_ll import DoublyLL
 
 
 class TestNormalCases(unittest.TestCase):
